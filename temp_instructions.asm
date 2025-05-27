1c595823898ab112a2e871b5463e600953d88174
AbdelrahmanSamha <AbdelrahmanSamha@outlook.com>
2025-05-27 22:00:45 +0300

    asd

--- temp_instructions.asm
+++ temp_instructions.asm
@@ -1,25 +1,21 @@
 .text
-main:
-addi $t0,$zero, 0x0
-addi $t1,$zero, 0x1
-addi $t2,$zero, 0x10
-addi $t3,$zero, 0x0
-addi $t4,$zero, 0x0
-addi $t6,$zero, 0x0
-store_loop:
-sw $t1, 0x0($t3)
-addi $t1, $t1, 0x1
-addi $t3, $t3, 0x1
-addi $t0, $t0, 0x1
-bne $t0, $t2, store_loop
-addi $t0,$zero, 0x0
-addi $t3,$zero, 0x0
-sum_loop:
-lw $t5, 0x0($t3)
-add $t4, $t4, $t5
-lw $t7, 0x1($t3)
-add $t6, $t6, $t7
-addi $t3, $t3, 0x1
-addi $t0, $t0, 0x2
-bne $t0, $t2, sum_loop
-add $s0,$t4, $t6
+addi $t0, $zero, 0x10
+addi $t1, $zero, 0x5
+or $t2, $t1, $t0
+xor $t3, $t1, $t0
+nor $t4, $t1, $t0
+slt $t5, $t1, $t0
+ori $s0, $zero, 0x10
+xori $s1, $zero, 0x10
+sll $s2, $t1, 0x4
+srl $s3, $t1, 0x4
+slti $s4, $t0, 0x20
+sgt $s5, $t0, $t1
+beq $t0, $t1, L
+bne $t0, $t1, lab
+slti $k0, $t0, 0x20
+sgt $k1, $t0, $t1
+L:
+addi $t8, $s5, 0x1
+lab:
+addi $t8, $s4, 0x2
